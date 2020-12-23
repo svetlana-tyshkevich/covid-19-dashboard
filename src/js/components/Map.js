3549d58d89d0dcf7288d0cf6ea56d0fa820b975f
Svetlana Tyshkevich <panfilova.ss@gmail.com>
2020-12-23 23:20:00 +0200

    fix: change repeating class and map zoom

--- src/js/components/Map.js
+++ src/js/components/Map.js
@@ -52,7 +52,7 @@ export default class WorldMap extends BaseComponent {
       container: 'mapBox',
       style: 'mapbox://styles/roachbu/ckir7pnl57ff717qvs68nrrvm',
       center: [53.9015, 27.566],
-      zoom: 1,
+      zoom: 0,
     });
     const popup = new mapboxgl.Popup({
       closeButton: false,
@@ -458,6 +458,8 @@ export default class WorldMap extends BaseComponent {
     if (target !== 'global') {
       this.flyToCountry(target);
       this.model.setState('country', target);
+    } else {
+      this.map.zoomTo(0, { duration: 2000 });
     }
   };
 
